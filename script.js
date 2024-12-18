d5789655c92ace3a5f1bafa3cb3c868a0534bfb9
D7ity <109730443+D7ity@users.noreply.github.com>
2024-12-18 14:49:54 +1100

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,4 @@
-const audio = document.getElementById('bg-audio');
-audio.volume = 0;
-
-document.body.addEventListener('click', () => {
-    audio.play(); // Start audio playback on user interaction
-    let volume = 0;
-    const targetVolume = 0.1;
-    const fadeInterval = 50;
-    const fadeStep = targetVolume / 20;
-
-    const fadeIn = setInterval(() => {
-        volume += fadeStep;
-        if (volume >= targetVolume) {
-            volume = targetVolume;
-            clearInterval(fadeIn);
-        }
-        audio.volume = volume;
-    }, fadeInterval);
-});
-
-const titleText = "deity ♡";
+const titleText = "deity";
 let titleIndex = 0;
 let isTyping = true;
 
